61d7bb35c67df0960aaaf93467cd8e5800795e66
XanderHeinen <x.l.heinen@student.utwente.nl>
2024-10-27 23:38:41 +0100

    Changelog: - updates to almost all html/css files for updated styling - added profile pages - added delete button to journal editing - added proper documentation for almost every method - testing methods for usercontroller and communityController - new Authenticationhandler - choose your own style for picture generation - more

---
+++ src/test/java/com/digitallife/journal_site/Journal/JournalControllerTest.java
@@ -0,0 +1,36 @@
+package com.digitallife.journal_site.Journal;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JournalControllerTest {
+
+    @Test
+    void saveJournalEntry() {
+    }
+
+    @Test
+    void getJournalEntries() {
+    }
+
+    @Test
+    void showUserJournals() {
+    }
+
+    @Test
+    void editJournalEntry() {
+    }
+
+    @Test
+    void updateJournalEntry() {
+    }
+
+    @Test
+    void getAllJournalEntries() {
+    }
+
+    @Test
+    void showImageTimeline() {
+    }
+}
